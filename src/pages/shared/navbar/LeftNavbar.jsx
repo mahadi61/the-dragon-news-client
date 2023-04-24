cc143e47d12a38ec5a3615c5a29f83fe67102689
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-04-24 12:28:42 +0600

    left nav done

--- src/pages/shared/navbar/LeftNavbar.jsx
+++ src/pages/shared/navbar/LeftNavbar.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNavbar = () => {
-  return <div>Left nav</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h4>All Category</h4>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link className="text-decoration-none text-black mx-3">
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNavbar;
